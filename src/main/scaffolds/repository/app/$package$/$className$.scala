75a548f658017a34eb9f19f2f3162cafc42c1359
Dennis Vriend <dnvriend@gmail.com>
2016-11-30 20:32:16 +0400

    update

--- src/main/scaffolds/repository/app/$package$/$className$.scala
+++ src/main/scaffolds/repository/app/$package$/$className$.scala
@@ -16,19 +16,19 @@ final case class $entityName$(id: Option[Long])
 class $className$ @Inject() (db: Database) {
    val logger = Logger(this.getClass)
 
-   def get$entityName$(limit: Int, offset: Int): List[$entityName$] = db.withConnection { implicit conn =>
+   def take(limit: Int, offset: Int): List[$entityName$] = db.withConnection { implicit conn =>
      SQL"SELECT * FROM $entityName;format="lower"$ limit=#\$limit, offset=#\$offset".as($entityName$.parser.*)
    }
 
-   def get$entityName$ById(id: Long): Option[$entityName$] = db.withConnection { implicit conn =>
+   def findById(id: Long): Option[$entityName$] = db.withConnection { implicit conn =>
      SQL"SELECT * FROM $entityName;format="lower"$ WHERE id=#\$id".as($entityName$.parser.singleOpt)
    }
 
-   def delete$entityName$($entityName;format="lower"$: $entityName$): Unit = db.withConnection { implicit conn =>
+   def delete($entityName;format="lower"$: $entityName$): Unit = db.withConnection { implicit conn =>
      SQL"DELETE FROM $entityName;format="lower"$ WHERE id=\${$entityName;format="lower"$.id}".executeUpdate
    }  
 
-   def add$entityName$($entityName;format="lower"$: $entityName$): $entityName$ = db.withConnection { implicit conn =>
+   def add($entityName;format="lower"$: $entityName$): $entityName$ = db.withConnection { implicit conn =>
     import anorm.SqlParser.long
     val theId = SQL"INSERT INTO $entityName;format="lower"$ (id) values (\${$entityName;format="lower"$.id})".executeInsert(long(1).single)
     $entityName;format="lower"$.copy(id = Option(theId))
