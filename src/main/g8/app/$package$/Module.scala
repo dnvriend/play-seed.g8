a2bac6d0f6b6952baa73224777ea3b79f9322b9b
Dennis Vriend <dnvriend@gmail.com>
2016-11-30 19:52:47 +0400

    update

--- src/main/g8/app/$package$/Module.scala
+++ src/main/g8/app/$package$/Module.scala
@@ -1,12 +1,9 @@
 package $package$
 
 import akka.util.Timeout
-import akka.event.{Logging, LoggingAdapter}
-import akka.pattern.CircuitBreaker
 import com.google.inject.name.Names
-import com.google.inject.{AbstractModule, Provider, Provides}
+import com.google.inject.AbstractModule
 import play.api.libs.concurrent.AkkaGuiceSupport
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 class Module extends AbstractModule with AkkaGuiceSupport {
@@ -17,10 +14,5 @@ class Module extends AbstractModule with AkkaGuiceSupport {
 
     bind(classOf[Timeout])
       .toInstance(Timeout(10.seconds))
-  }
-
-  @Provides
-  def loggingAdapter(system: ActorSystem): LoggingAdapter = {
-    Logging(system, this.getClass)
   }
 }
