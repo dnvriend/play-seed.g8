1d5084bf86262e23b89517cef1e4194217fedca4
Dennis Vriend <dnvriend@gmail.com>
2016-12-01 22:18:47 +0400

    update

--- src/main/g8/app/$package$/component/kafka/repository/KafkaRepository.scala
+++ src/main/g8/app/$package$/component/kafka/repository/KafkaRepository.scala
@@ -12,10 +12,10 @@ class KafkaRepository @Inject() (db: Database) {
   val logger = Logger(this.getClass)
 
   def updateOffset(topic: String, groupId: String, partition: Int, offset: Long): Try[Int] = db.withConnection { implicit conn =>
-    Try(SQL"UPDATE kafka_offsets SET topic_offset=#$offset WHERE topic='#$topic' AND group_id='#$groupId' AND partition=#$partition".executeUpdate()).recoverWith {
+    Try(SQL"UPDATE kafka_offsets SET topic_offset=#\$offset WHERE topic='#\$topic' AND group_id='#\$groupId' AND partition=#\$partition".executeUpdate()).recoverWith {
       case t: Throwable =>
         logger.error("Could not update offset", t)
-        Try(SQL"INSERT INTO kafka_offsets (group_id, topic, partition, topic_offset) VALUES ('#$groupId', '#$topic', #$partition, #$offset)".executeUpdate()).recoverWith {
+        Try(SQL"INSERT INTO kafka_offsets (group_id, topic, partition, topic_offset) VALUES ('#\$groupId', '#\$topic', #\$partition, #\$offset)".executeUpdate()).recoverWith {
           case t: Throwable =>
             logger.error("Could not insert offset", t)
             Failure(t)
@@ -24,6 +24,6 @@ class KafkaRepository @Inject() (db: Database) {
   }
 
   def findOffset(topic: String, groupId: String, partition: Int): Option[Long] = db.withConnection { implicit conn =>
-    SQL"SELECT topic_offset FROM kafka_offsets WHERE topic='#$topic' AND group_id='#$groupId' AND partition=#$partition".as(SqlParser.scalar[Long].singleOpt)
+    SQL"SELECT topic_offset FROM kafka_offsets WHERE topic='#\$topic' AND group_id='#\$groupId' AND partition=#\$partition".as(SqlParser.scalar[Long].singleOpt)
   }
 }
